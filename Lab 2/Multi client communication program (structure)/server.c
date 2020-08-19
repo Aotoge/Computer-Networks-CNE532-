1f2604ba5f7987b64586f3a3a16387a34b6cf30b
Kadambari Anuranjana <kadambari.anuranjana@gmail.com>
2020-08-19 21:10:19 +0530

    Add server.c for Lab 2 Question 2

---
+++ Lab 2/Multi client communication program (structure)/server.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+
+struct data{
+	int a;
+	char b;
+	float c;
+};
+
+int main(int argc, char* argv[]){
+	
+	int fd_from, fd_to;
+	struct sockaddr_in server, from, to;
+	int fromlen;
+	int temp;
+	struct data d;
+
+	fd_from = socket(AF_INET,SOCK_DGRAM,0);
+	if(fd_from<0){
+		printf("Error creating socket\n");
+		exit(1);
+	}
+
+	bzero((void*)&server,sizeof(server));
+	server.sin_family=AF_INET;
+	server.sin_port=htons(atoi(argv[1]));
+	server.sin_addr.s_addr=INADDR_ANY;
+
+	temp = bind(fd_from,(struct sockaddr*)&server,sizeof(server));
+	if(temp<0){
+		printf("Error binding\n");
+		exit(1);
+	}
+
+	int n = sizeof(struct data);
+	temp=recvfrom(fd_from,(char*)&d,n,0,(struct sockaddr*)&from,&fromlen);
+	/*if(temp<0){
+		printf("%d%c%f Error Recieving\n",d.a,d.b,d.c);
+		exit(1);
+	}*/
+	
+	//Making edits to data
+	d.a++;
+	d.b++;
+	d.c-=0.5;
+	printf("Sending: %d %c %f\n",d.a,d.b,d.c);
+	close(fd_from);
+	
+	//send portion
+	fd_to=socket(AF_INET, SOCK_DGRAM, 0);
+	if(fd_to<0){
+		printf("Error creating socket");
+		exit(1);
+	}
+	int t = sizeof(struct sockaddr_in);
+	to.sin_family = AF_INET;
+	to.sin_port = htons(atoi(argv[3]));
+	struct hostent *host = gethostbyname(argv[2]);
+	if(host == 0){
+		printf("Hostname error");
+		exit(1);
+	}
+	bcopy((char*)host->h_addr,(char*)&to.sin_addr,host->h_length);
+	temp=sendto(fd_to,(char*)&d,n,0,(struct sockaddr*)&to,t);
+	if(temp<0){
+		printf("Error Sending");
+		exit(1);
+	}
+	close(fd_to);
+
+	return 0;
+}
